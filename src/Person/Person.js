3b3ccec76be362fa26e1b646353198888df7a31c
Efi Garofalaki <eufrosunigarofalaki@gmail.com>
2020-09-29 20:59:11 +0300

    a small change for test

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,10 +5,11 @@ const person = (props) => {
         return (
             <div className="Person">
                 <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+                <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
                 <p>{props.children}</p>
                 <input type="text" onChange={props.changed} value={props.name}/>
             </div>
-        bvbvc
+        
         );  
 };
 
