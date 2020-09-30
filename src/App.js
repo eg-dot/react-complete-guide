2f946d84058751c7e67072db4dbf309f577067b1
eg-dot <eufrosunigarofalaki@gmail.com>
2020-09-30 15:02:11 +0300

    class 54

--- src/App.js
+++ src/App.js
@@ -50,29 +50,33 @@ togglePersonsHandler = () => {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}>My hobbies: Racing</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+        </div>       
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi React app</h1>
         <button 
         style={style} 
         onClick={this.togglePersonsHandler}>Switch name</button>
-        { 
-          this.state.showPersons ?
-            <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangedHandler}>My hobbies: Racing</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}/>
-          </div> : null
-        }
-        
+        {persons}
       </div>
     );
   }
